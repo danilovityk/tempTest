1549c2e9d59c17d901ab6235e920b13fa62900b2
danilovityk <danilovityk@gmail.com>
2024-04-27 19:25:32 -0400

    initial commit

--- components/Main.js
+++ components/Main.js
@@ -4,11 +4,10 @@ export default function Main() {
       <div className="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
           <h1 className="mb-5 sm:text-6xl text-5xl items-center Avenir xl:w-2/2 text-gray-900">
-            We are making Stunning Websites
+          Big dreams start here!
           </h1>
           <p className="mb-4 xl:w-3/4 text-gray-600 text-lg">
-            nine4 is a free to use website template for websites made with
-            Next.js and styled with Tailwind CSS
+          As the Global Leading OnlyFans Agency in USA, we work with motivated Content Creators and Influencers to unlock the unfathomable potential of reach and revenue.
           </p>
           <div className="flex justify-center">
             <a
@@ -22,8 +21,8 @@ export default function Main() {
         <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
           <img
             className="w-80 md:ml-1 ml-24"
-            alt="iPhone-12"
-            src="/images/iPhone-12-Mockup.png"
+            alt="iPhone-12-cooked"
+            src="images/iPhoneHuy.jpg"
           ></img>
         </div>
       </div>
@@ -37,28 +36,28 @@ export default function Main() {
           <div className="grid grid-cols-2 gap-16 mb-16 text-center lg:grid-cols-4">
             <div className="flex items-center justify-center">
               <img
-                src="/images/Google-Logo.webp"
+                src="/images/Onlyfans-Logo.png"
                 alt="Google Logo"
                 className="block object-contain h-16 greyC"
               ></img>
             </div>
             <div className="flex items-center justify-center">
               <img
-                src="/images/Shopify-Logo.svg"
+                src="/images/StayUndercover.png"
                 alt="Shopify Logo"
                 className="block object-contain h-16 greyC"
               ></img>
             </div>
             <div className="flex items-center justify-center">
               <img
-                src="/images/Cloudflare-Logo.svg"
+                src="/images/CreatorHeroBlack.png"
                 alt="Cloudflare Logo"
                 className="block object-contain h-16 greyC"
               ></img>
             </div>
             <div className="flex items-center justify-center">
               <img
-                src="/images/PayPal-Logo.png"
+                src="/images/repurpose_io.webp"
                 alt="Paypal Logo"
                 className="block object-contain h-16 greyC"
               ></img>
