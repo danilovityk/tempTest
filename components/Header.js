70fb8a33a4cd32a6c4cd86aaf055badef75202ef
danilovityk <danilovityk@gmail.com>
2024-04-28 01:14:59 -0400

    Pic updated, footer part removed

--- components/Header.js
+++ components/Header.js
@@ -68,7 +68,7 @@ export default function Header() {
                   className="inline-flex items-center px-4 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-gray-900"
                   href="/"
                 >
-                  <span className="justify-center">Download</span>
+                  <span className="justify-center">Become my bitch</span>
                   <svg
                     className="w-3 h-3 fill-current text-gray-400 flex ml-2 -mr-1"
                     viewBox="0 0 12 12"
